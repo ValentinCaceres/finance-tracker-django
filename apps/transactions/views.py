5fa1c98f96f999c95fe3592f93face796463d01c
ValentinCaceres <valentin@ademsys.com>
2026-02-03 01:09:34 -0300

    Progress #1 Transactions List

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
 
 # Create your views here.
+class TransactionListView(
+    LoginRequiredMixin,
+    ListView
+):
+    model = Transaction
+    template_name = 'transactions/transaction_list.html'
+    context_object_name = 'transactions'
+    ordering = ['-transaction_date']
+    paginate_by = 20
+
+    def get_queryset(self):
+        return Transaction.objects.filter(
+            user=self.request.user
+        ).select_related(
+            'category',
+            'account',
+            'destination_account'
+        )
+    
